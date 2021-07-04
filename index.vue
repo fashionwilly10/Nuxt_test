688a5a1dc81e51118d34996a908ade21bd951a1b
fashionwilly10 <41353352+fashionwilly10@users.noreply.github.com>
2021-07-05 00:19:18 +0800

    Add files via upload

---
+++ index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <!--header-->
+    <header class="bar">
+        <form name="search" class="search" id="search" action="">
+            <div class="bar_block"> 
+              <input type="search" id="search" class="search-bar" placeholder="Search.." />
+           	</div> 
+        </form>
+    </header>
+ 
+    <article>
+      <section id="content">
+        <img src="~/static/images/cat.jpg" >
+        <img src="~/static/images/cat.jpg" >
+        <img src="~/static/images/cat.jpg" >
+        <img src="~/static/images/cat.jpg" >
+        <img src="~/static/images/cat.jpg" >
+        <img src="~/static/images/cat.jpg" >
+        <img src="~/static/images/cat.jpg" >
+        <img src="~/static/images/cat.jpg" >
+      </section>
+    </article>
+
+    <!--footer-->
+    <footer class="tool">
+      <ul>
+        <li><a href="#">tool 1</a></li>
+        <li><a href="#">tool 2</a></li>
+        <li><a href="#">tool 3</a></li>
+      </ul>
+    </footer>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  
+</style>
